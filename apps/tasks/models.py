284bf2ea4c014d2192dfe3c0820a0f607935a721
Davimteixeira <daviteixeira077@gmail.com>
2024-08-14 15:32:06 -0300

    feat: criando model de Task

--- apps/tasks/models.py
+++ apps/tasks/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Task(models.Model):
+    title = models.CharField(max_length=255) 
+    description = models.TextField() 
+    due_date = models.DateField()  #YYYY-MM-DD
+    created_at = models.DateTimeField(auto_now_add=True)  
+    updated_at = models.DateTimeField(auto_now=True) 
+
+    class Meta:
+        verbose_name = 'Task'
+        verbose_name_plural = 'Tasks'
+
+    def __str__(self):
+        return self.title 
