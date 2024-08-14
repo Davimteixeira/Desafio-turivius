42d12900170af3bf4132c80da999178f479d070d
Davimteixeira <daviteixeira077@gmail.com>
2024-08-14 15:34:36 -0300

    feat: criando serializer e viewset de tasks

--- apps/tasks/views.py
+++ apps/tasks/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from rest_framework import filters
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from rest_framework import status
 
-# Create your views here.
+from .filters import TaskFilter
+from .models import Task
+from .serializers import TaskSerializer
+
+class TaskViewSet(ModelViewSet):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+    permission_classes = [IsAuthenticated]
+    filter_backends = [TaskFilter, filters.OrderingFilter] 
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        self.perform_destroy(instance)
+        return Response({"message": "Task deleted successfully."}, status=status.HTTP_200_OK)
