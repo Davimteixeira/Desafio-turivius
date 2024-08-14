2d76d243dd34a23a85a94ea4dad11643f05a3059
Davimteixeira <daviteixeira077@gmail.com>
2024-08-14 11:58:30 -0300

    feat: criando model de CustomUser

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class CustomUser(AbstractUser):
+
+    is_first_login = models.BooleanField(default=True)
+    email = models.EmailField(verbose_name='email address', max_length=255, unique=True, blank=True, null=True)
+
+    class Meta:
+        verbose_name_plural = "Usuários"
