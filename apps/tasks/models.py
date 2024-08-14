217ba5d66479b84f4793840240c5d12cd2e7f89a
Davimteixeira <daviteixeira077@gmail.com>
2024-08-14 16:26:31 -0300

    feat: implementando o soft delete

--- apps/tasks/models.py
+++ apps/tasks/models.py
@@ -1,15 +1,45 @@
 from django.db import models
 
+class TaskQuerySet(models.QuerySet):
+    def active(self):
+        return self.filter(is_deleted=False)
+
 class Task(models.Model):
     title = models.CharField(max_length=255) 
     description = models.TextField() 
     due_date = models.DateField()  #YYYY-MM-DD
     created_at = models.DateTimeField(auto_now_add=True)  
-    updated_at = models.DateTimeField(auto_now=True) 
+    updated_at = models.DateTimeField(auto_now=True)
+    is_deleted = models.BooleanField(default=False)
+
+    # Usar o gerenciador personalizado
+    objects = TaskQuerySet.as_manager() 
+    active_objects = TaskQuerySet.as_manager()  # Gerenciador para objetos ativos
+
+    def delete(self):
+        """
+        Realiza o soft delete, marcando a task como deletada.
+        """
+        self.is_deleted = True
+        self.save()
+
+    def hard_delete(self):
+        """
+        Remove permanentemente o objeto do banco de dados.
+        """
+        super(Task, self).delete()
+
+    def restore(self):
+        """
+        Restaura um objeto deletado logicamente, marcando-o como não deletado.
+        """
+        self.is_deleted = False
+        self.save()
 
     class Meta:
-        verbose_name = 'Task'
-        verbose_name_plural = 'Tasks'
+        verbose_name = "Task"  
+        verbose_name_plural = "Tasks"  
+        db_table = 'tasks'  
 
     def __str__(self):
-        return self.title 
+        return self.title
--- apps/tasks/views.py
+++ apps/tasks/views.py
@@ -1,20 +1,40 @@
-from rest_framework.viewsets import ModelViewSet
-from rest_framework import filters
+from rest_framework import viewsets, filters, status
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
-from rest_framework import status
-
-from .filters import TaskFilter
 from .models import Task
 from .serializers import TaskSerializer
+from .filters import TaskFilter
+from rest_framework.decorators import action
 
-class TaskViewSet(ModelViewSet):
+
+class TaskViewSet(viewsets.ModelViewSet):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
     permission_classes = [IsAuthenticated]
-    filter_backends = [TaskFilter, filters.OrderingFilter] 
+    filter_backends = [TaskFilter, filters.OrderingFilter]
+
+    def get_queryset(self):
+        # Retorna apenas as tasks que não foram deletadas logicamente
+        return Task.objects.active()
 
     def destroy(self, request, *args, **kwargs):
+        """
+        Realiza o soft delete da task.
+        """
         instance = self.get_object()
-        self.perform_destroy(instance)
+        instance.delete() 
         return Response({"message": "Task deleted successfully."}, status=status.HTTP_200_OK)
+
+    
+    @action(detail=True, methods=['post'], serializer_class=None)
+    def restore(self, request, pk=None):
+        """
+        Restaura uma task deletada logicamente.
+        """
+        try:
+            
+            instance = Task.objects.get(pk=pk, is_deleted=True)  
+            instance.restore() 
+            return Response({"message": "Task restored successfully."}, status=status.HTTP_200_OK)
+        except Task.DoesNotExist:
+            return Response({"error": "Task not found or not deleted."}, status=status.HTTP_404_NOT_FOUND)
