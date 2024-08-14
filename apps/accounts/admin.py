4326f3f75ae9bb923c42671aa260863b079e4f78
Davimteixeira <daviteixeira077@gmail.com>
2024-08-14 12:57:13 -0300

    feat: criando o admin de accounts

--- apps/accounts/admin.py
+++ apps/accounts/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .models import *
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+
+    fieldsets = UserAdmin.fieldsets + (
+            (None, {'fields': ('type','is_first_login')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'password1', 'password2', 'type'),
+        }),
+    )
+
+admin.site.register(CustomUser, CustomUserAdmin)
